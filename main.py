e48233a85dbbe45d05b9ac54c2dea7a07a45d73b
Antonio Drumond <adcdstom@gmail.com>
2025-06-04 09:24:16 -0300

    subjanelas + titulo

--- main.py
+++ main.py
@@ -2,6 +2,12 @@ import tkinter as tk
 from tkinter import ttk
 
 
+def destroy_all():
+    root.destroy()
+    buckets.destroy()
+    directory.destroy()
+
+
 def insert_key():
     var = var_entry.get()
     var_entry.delete(0, 10000)
@@ -29,9 +35,26 @@ def remove_key():
 root = tk.Tk()
 root.title("Visualizador Hash Extensivel")
 root.geometry("100x300+500+100")
+## Directory
+directory = tk.Tk()
+directory.title("Diretório")
+directory.geometry("200x400+800+150")
+## Buckets
+buckets = tk.Tk()
+buckets.title("Buckets")
+buckets.geometry("500x200+1300+150")
+
+# Define labels
+title = tk.Label(text="Visualizador\nde Hash\nExtensivel")
 
 # Define buttons
-close = tk.Button(root, text="Quit", command=root.destroy)
+
+close = tk.Button(
+    root,
+    text="Quit",
+    # command=root.destroy
+    command=destroy_all,
+)
 
 var_entry = ttk.Entry(root)
 
@@ -48,10 +71,12 @@ rm_button = tk.Button(
 )
 
 # Pack buttons
+title.pack()
 var_entry.pack()
-ins_button.pack()
-rm_button.pack()
+ins_button.pack(fill=tk.X)
+rm_button.pack(fill=tk.X)
 close.pack(side=tk.BOTTOM)
 
 # Initialize main screen
 root.mainloop()
+buckets.mainloop()
