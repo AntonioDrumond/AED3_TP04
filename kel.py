2743f72b6338a8d316a6d32ef6dd9a04ac6c5e64
Raquel <apxraquel@gmail.com>
2025-06-05 09:50:36 -0300

    mostra o resultado da funcao hash para o valor inserido na root

--- kel.py
+++ kel.py
@@ -48,15 +48,23 @@ def main():
 
     root = tk.Tk()
     root.title("Visualizador Hash Extensível")
-    #root.geometry("260x360+300+120")
+    
+    root_header_frame = ttk.Frame(root)
+    root_header_frame.pack(fill="x", padx=8, pady=(8, 0))
+    
+    hash_label = ttk.Label(
+        root_header_frame,
+        text="Hash: - % 2^p = -",  # Initial state
+        anchor="center",
+        font=("Arial", 12)
+    )
+    hash_label.pack(pady=10)
 
     dir = tk.Toplevel(root)
     dir.title("Diretório")
-    #dir.geometry("260x360+590+120")
 
     buckets = tk.Toplevel(root)
     buckets.title("Buckets")
-    #buckets.geometry("620x280+880+120")
     
     # Center and size all windows
     center_windows(root, dir, buckets)
@@ -90,7 +98,7 @@ def main():
     tabela_dir = Table(dir_table_frame, headers=headersD)
     tabela_dir.add_row();
 
-    # ---------------- controles ----------------
+    # ---------------- INSERÇÃO ----------------
     
     def update_profundidadeDIR():
         x = he.dir_size
@@ -99,33 +107,17 @@ def main():
     frm_controls = ttk.LabelFrame(root, text="Inserir valor")
     frm_controls.pack(fill="x", padx=8, pady=8)
 
-    #ttk.Label(frm_controls, text="Linha:").grid(row=0, column=0, padx=2, pady=4, sticky="e")
-    #spin_row = tk.Spinbox(frm_controls, from_=1, to=tabela.rows, width=5, justify="center")
-    #spin_row.grid(row=0, column=1, padx=2, pady=4)
-
-    #ttk.Label(frm_controls, text="Coluna:").grid(row=0, column=2, padx=2, pady=4, sticky="e")
-    #spin_col = tk.Spinbox(frm_controls, from_=1, to=tabela.cols, width=5, justify="center")
-    #spin_col.grid(row=0, column=3, padx=2, pady=4)
-
     ttk.Label(frm_controls, text="Valor:").grid(row=1, column=0, padx=2, pady=4, sticky="e")
     entry_val = ttk.Entry(frm_controls, width=18)
     entry_val.grid(row=1, column=1, columnspan=3, padx=2, pady=4, sticky="we")
-
-    #def mandar_pra_tabela(x, y, valor):
-        #try:
-            #linha   = int(x)
-            #coluna  = int(y)
-            #if valor == "":
-                #raise ValueError("Valor vazio")
-            #tabela.set_value(linha, coluna, valor)
-            #entry_val.delete(0, tk.END)
-        #except (ValueError, IndexError) as err:
-            #messagebox.showerror("Erro", f"Entrada inválida:\n{err}")
             
     def inserir_valor():
         valor = entry_val.get()
         try:
             valor = int(valor)
+            
+            resultado_hash = he.hash(valor)
+            
             he.insert(valor)
             tab = formatar_tabela()
             tabela.update_data(tab)
@@ -137,7 +129,10 @@ def main():
             except Exception as e:
                 messagebox.showerror("Erro DIR", f"Erro ao atualizar diretório: {str(e)}")
                 
+            hash_label.config(text=f"Hash: {valor} % 2^p = {resultado_hash}")
+            
             entry_val.delete(0, tk.END)
+            
         except ValueError:
             messagebox.showerror("Erro", "Entrada inválida: deve ser um número inteiro")
     
@@ -181,19 +176,57 @@ def main():
 
     def add_new_row():
         tabela.add_row()
-        spin_row.config(to=tabela.rows)
+        spin_row.config(to=tabela.rows)       
 
-    #ttk.Button(root, text="Adicionar Linha", command=add_new_row)\
-            #.pack(fill="x", padx=8, pady=4)
-
+    # ---------------- BUSCA ----------------
+    
+    def buscar_valor():
+        x = 2
+    
+    frm_controls = ttk.LabelFrame(root, text="Buscar valor")
+    frm_controls.pack(fill="x", padx=8, pady=8)
+    
+    ttk.Label(frm_controls, text="Valor:").grid(row=1, column=0, padx=2, pady=4, sticky="e")
+    entry_search = ttk.Entry(frm_controls, width=18)
+    entry_search.grid(row=1, column=1, columnspan=3, padx=2, pady=4, sticky="we")
+    
+    ttk.Button(frm_controls, text="Buscar", command=buscar_valor)\
+            .grid(row=2, column=0, columnspan=4, pady=(6, 4), sticky="we")
+    
+    
     ttk.Button(root, text="Sair", command=root.destroy)\
             .pack(fill="x", padx=8, pady=(0, 8))
-
+    
     root.mainloop()
 
 if __name__ == "__main__":
     main()
 
+# ANOTAÇOESSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSss
 
 #You can also add pre-filled rows by passing data to add_row(), like:
 #tabela.add_row([6, "New Name", "New City", 25])
+
+#ttk.Label(frm_controls, text="Linha:").grid(row=0, column=0, padx=2, pady=4, sticky="e")
+#spin_row = tk.Spinbox(frm_controls, from_=1, to=tabela.rows, width=5, justify="center")
+#spin_row.grid(row=0, column=1, padx=2, pady=4)
+
+#ttk.Label(frm_controls, text="Coluna:").grid(row=0, column=2, padx=2, pady=4, sticky="e")
+#spin_col = tk.Spinbox(frm_controls, from_=1, to=tabela.cols, width=5, justify="center")
+#spin_col.grid(row=0, column=3, padx=2, pady=4)
+
+#ttk.Button(root, text="Adicionar Linha", command=add_new_row)\
+        #.pack(fill="x", padx=8, pady=4)
+        
+#def mandar_pra_tabela(x, y, valor):
+    #try:
+        #linha   = int(x)
+        #coluna  = int(y)
+        #if valor == "":
+            #raise ValueError("Valor vazio")
+        #tabela.set_value(linha, coluna, valor)
+        #entry_val.delete(0, tk.END)
+    #except (ValueError, IndexError) as err:
+        #messagebox.showerror("Erro", f"Entrada inválida:\n{err}")
+    
+    #root.geometry("260x360+300+120")
